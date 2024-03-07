efa70f8def69ceb8327421908fbdec8639bccf85
Maico Alexandre Kley <maicokley@hotmail.com>
2024-03-07 01:31:25 +0000

    adiciona script `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Voltando a subir algum projeto no github.
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Olha pra mim :v</h1>
+  return <h1>Olha pra mim :v</h1>;
 }
 
 export default Home;

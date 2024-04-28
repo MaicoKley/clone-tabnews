23b072a118394048f424505f21d92bb502fd904e
MaicoKley <maicokley@hotmail.com>
2024-04-28 10:52:24 -0300

    remove linha em branco

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -4,7 +4,6 @@ import database from "infra/database";
 
 export default async function migrations(request, response) {
   const dbClient = await database.getNewClient();
-
   const defaultMigrationOptions = {
     dbClient: dbClient,
     dryRun: true,
